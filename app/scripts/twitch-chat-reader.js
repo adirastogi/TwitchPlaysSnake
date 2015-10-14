0bb1d4144a4490b5b1696d3021a64a24bbc98c77
Eric Steele <ericrsteele@gmail.com>
2015-10-13 19:03:31 -0700

    Added more test cases for the Twitch Chat Reader.

--- app/scripts/twitch-chat-reader.js
+++ app/scripts/twitch-chat-reader.js
@@ -3,20 +3,28 @@
 
 var TwitchChatReader = (function () {
 
+  var inputMap = {
+    'UP':    'UP',
+    'DOWN':  'DOWN',
+    'LEFT':  'LEFT',
+    'RIGHT': 'RIGHT',
+    'W':     'UP',
+    'S':     'DOWN',
+    'A':     'LEFT',
+    'D':     'RIGHT'
+  };
+
   return {
     read: read
   };
 
-  function read(channel, user, message) {
-    message = message.trim().toUpperCase();
-
-    if (isAction(message)) {
-      TwitchActionQueue.push({user: user, action: message});
+  function read(channel, user, input) {
+    input = input.trim();
+    var action = inputMap[input.toUpperCase()];
+    if (action) {
+      var now = new moment();
+      TwitchActionQueue.push({user: user, action: action, input: input, timestamp: now.format("HH:mm:ss")});
     }
-  }
-
-  function isAction(message) {
-    return message === 'UP' || message === 'DOWN' || message === 'LEFT' || message === 'RIGHT';
   }
 
 })();
