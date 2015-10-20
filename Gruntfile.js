33e48a7da005ba14674c1d14ba550297dca8db8c
Eric Steele <ericrsteele@gmail.com>
2015-10-19 22:23:15 -0700

    Removed comment from Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,9 +34,6 @@ module.exports = function (grunt) {
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
-     // options: {
-     //   livereload: true
-    //  },
       express: {
         files:  ['./data-logger.js'],
         tasks:  ['express:dev'],
